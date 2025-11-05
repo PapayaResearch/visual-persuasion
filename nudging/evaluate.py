0222e7ba51a3ac2a7338c0c1872ecaaece48e99b
Pranav M R <148135964+pranav-m-r@users.noreply.github.com>
2025-11-05 15:36:18 +0530

    Generalized evaluation pipeline

--- nudging/evaluate.py
+++ nudging/evaluate.py
@@ -1,9 +1,10 @@
 import os
+import re
 import logging
 import random
 import itertools
 import pandas as pd
-from typing import List
+from typing import List, Tuple
 from collections import defaultdict
 from utils.wrappers import LanguageModel
 
@@ -14,30 +15,47 @@ class EvaluationPipeline:
     def __init__(
         self,
         evaluator_prompt: str,
-        evaluator_model: LanguageModel
+        evaluator_model: LanguageModel,
+        allow_same_image_comparison: bool,
+        only_allow_same_image_comparison: bool,
+        only_allow_first_last_comparison: bool
     ):
         self.evaluator_prompt = evaluator_prompt
         self.evaluator_model = evaluator_model
-    
+        self.allow_same_image_comparison = allow_same_image_comparison
+        self.only_allow_same_image_comparison = only_allow_same_image_comparison
+        self.only_allow_first_last_comparison = only_allow_first_last_comparison
+
+    def _parse_filename(self, filename: str) -> Tuple[str, str, str, int]:
+        """
+        Parse filename to extract class_name, image_id, edit_type, and prior_index.
+        """
+        match = re.match(r'([A-Za-z0-9_]+)_([A-Za-z0-9]+)_([A-Za-z0-9-]+)\.jpg', filename)
+
+        class_name = match.group(1)
+        image_id = match.group(2)
+        edit_type = match.group(3)
+
+        return class_name, image_id, edit_type
+
     def _get_images_to_evaluate(self, image_paths: List[str]) -> List[str]:
         """
         Retrieves all image files to be evaluated based on naming conventions.
         """
-        images_to_evaluate = []
-        last_iteration = {}
+        if not self.only_allow_first_last_comparison:
+            return image_paths
 
+        first_iteration, last_iteration = {}, {}
         for img_path in image_paths:
-            filename = os.path.basename(img_path)
-            name_without_ext = os.path.splitext(filename)[0]
-            split = name_without_ext.split('_')
-            image_name = '_'.join(split[:-1])
-            version = split[-1]
-            if (image_name not in last_iteration) or (filename > last_iteration[image_name]):
-                last_iteration[image_name] = filename
-            if 'iter-0-original' in version or 'iter-1-edit' in version:
-                images_to_evaluate.append(filename)
-        
-        images_to_evaluate.extend(last_iteration.values())
+            if not img_path.lower().endswith('.jpg'):
+                continue
+            _, image_id, _ = self._parse_filename(os.path.basename(img_path))
+            if (image_id not in first_iteration) or (img_path < first_iteration[image_id]):
+                first_iteration[image_id] = img_path
+            if (image_id not in last_iteration) or (img_path > last_iteration[image_id]):
+                last_iteration[image_id] = img_path
+
+        images_to_evaluate = list(first_iteration.values()) + list(last_iteration.values())
 
         return images_to_evaluate
 
@@ -49,23 +67,21 @@ class EvaluationPipeline:
         results_data = []
 
         # Group images by class
-        class_groups = defaultdict(list)
+        class_groups = defaultdict(set)
         images_to_evaluate = self._get_images_to_evaluate(image_paths)
-        for img_path in image_paths:
+        for img_path in images_to_evaluate:
+            with open(img_path, "rb") as f:
+                    img_bytes = f.read()
             filename = os.path.basename(img_path)
-            if filename in images_to_evaluate:
-                name_without_ext = os.path.splitext(filename)[0]
-                split = name_without_ext.split('_')
-                image_class = split[0]
-                base = '_'.join(split[1:])
-                class_groups[image_class].append((img_path, base))
+            class_name, image_id, edit_type = self._parse_filename(filename)
+            class_groups[class_name].add((image_id, edit_type, img_bytes))
 
         logging.info(f"Found {len(class_groups)} image classes to evaluate\n")
 
         class_list = sorted(class_groups.keys())
 
         for image_class in class_list:
-            comparable_images = sorted(class_groups[image_class], key=lambda x: x[1])
+            comparable_images = sorted(class_groups[image_class], key=lambda x: '_'.join(x[:2]))
 
             if len(comparable_images) < 2:
                 logging.info(f"Only 1 image found for class {image_class}. Skipping evaluation.\n")
@@ -82,20 +98,27 @@ class EvaluationPipeline:
             all_evaluations += "-" * 40 + "\n\n"
 
             # Perform round-robin comparison
-            for (img1_path, base1), (img2_path, base2) in itertools.combinations(comparable_images, 2):
-                logging.info(f"\n--- Evaluating {base1} vs {base2} ---\n")
-                all_evaluations += f"Evaluating {base1} vs {base2}\n"
+            for (image_id_1, edit_type_1, img_bytes_1), (image_id_2, edit_type_2, img_bytes_2)\
+                in itertools.combinations(comparable_images, 2):
+                # Skip comparison if comparisons between same image IDs are not allowed
+                if (not self.allow_same_image_comparison) and (image_id_1 == image_id_2):
+                    continue
+                # Skip comparison if only comparisons between same image IDs are allowed
+                if (self.allow_same_image_comparison and
+                    self.only_allow_same_image_comparison and (image_id_1 != image_id_2)):
+                    continue
 
-                with open(img1_path, "rb") as f:
-                    img1_bytes = f.read()
-                with open(img2_path, "rb") as f:
-                    img2_bytes = f.read()
+                base_1 = f"{image_id_1}_{edit_type_1}"
+                base_2 = f"{image_id_2}_{edit_type_2}"
+
+                logging.info(f"\n--- Evaluating {base_1} vs {base_2} ---\n")
+                all_evaluations += f"Evaluating {base_1} vs {base_2}\n"
 
                 # Randomly decide which image is first and which is second to avoid bias
                 is_img1_first = random.choice([True, False])
 
-                image1_bytes = img1_bytes if is_img1_first else img2_bytes
-                image2_bytes = img2_bytes if is_img1_first else img1_bytes
+                image1_bytes = img_bytes_1 if is_img1_first else img_bytes_2
+                image2_bytes = img_bytes_2 if is_img1_first else img_bytes_1
 
                 # Evaluate the images without telling the VLM which is which
                 evaluation, usage = self.evaluator_model.get_response(
@@ -105,7 +128,7 @@ class EvaluationPipeline:
 
                 if not evaluation:
                     logging.error("Evaluation failed. Skipping to next comparison.\n")
-                    all_evaluations += f"ERROR: Evaluation failed for {base1} vs {base2}. Skipping.\n\n"
+                    all_evaluations += f"ERROR: Evaluation failed for {base_1} vs {base_2}. Skipping.\n\n"
                     continue
 
                 vlm_choice = evaluation.choice.lower()
@@ -114,9 +137,9 @@ class EvaluationPipeline:
                 img1_chosen = ((vlm_choice == "first" and is_img1_first) or
                                (vlm_choice == "second" and not is_img1_first))
 
-                choice = base1 if img1_chosen else base2
-                first = base1 if is_img1_first else base2
-                second = base2 if is_img1_first else base1
+                choice = base_1 if img1_chosen else base_2
+                first = base_1 if is_img1_first else base_2
+                second = base_2 if is_img1_first else base_1
 
                 result = (
                     f"Choice: {choice}\n"
@@ -139,8 +162,8 @@ class EvaluationPipeline:
                 # Add record to results data
                 results_data.append({
                     'image_class': image_class,
-                    'base1': base1,
-                    'base2': base2,
+                    'base1': base_1,
+                    'base2': base_2,
                     'choice': choice,
                     'reason': vlm_reason,
                     'first': first,
